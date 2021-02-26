6689fb79078f4a3f7434faea5f3bcb282e9e6111
HelenKolesnikovich <solnechnaya_88@list.ru>
2021-02-27 00:04:06 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let numberString = ''+ n;
+    let digitsArray = numberString.split('');
+    let reverseArray = [];
+    digitsArray.forEach(function(element){
+      reverseArray.unshift(element);
+    });
+    const reverseValue = reverseArray.join(''); 
+    const reverseNumber = parseInt(reverseValue, 10);
+    return reverseNumber;
 }
